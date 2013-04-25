3c3f66be7f869bd7d182bb230aa307bbe2f7ea45
jaecheol <jaecheol@captv.co.kr>
2013-04-25 10:14:42 +0900

    TestContentDbContext 에 데이터 입력 성공.

--- DRCY/Program.cs
+++ DRCY/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DRCY
 {
+    public class TestContent : DbContext
+    {
+        public DbSet<Episode> Episodes { get; set; }
+    }
+
+    public class Episode
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            using (var ctx = new TestContent())
+            {
+                ctx.Episodes.Add(new Episode() {Id = 100, Title = "(EBS)다큐10+"});
+
+                ctx.SaveChanges();
+            }
         }
     }
 }
